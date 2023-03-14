fcdb095155971dac29367fab88803773b4d12961
johnblog <wuqehilu@kellychibale-researchgroup-uct.com>
2023-03-14 11:51:50 +0300

    modifications 14-03-2023

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from . import models
+from django.contrib.auth.models import User
+from .models import *
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'index.html')
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)
+    return render(request, 'post_detail.html', {'post': post})
